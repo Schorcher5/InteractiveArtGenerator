ef254f3b81be9ef7d770b3ad1fcda30fd8c05aba
JoaquinCisneros <2014joaquincisneros@gmail.com>
2022-06-21 20:14:47 -0700

    Added some documentation to js,ejs, and readme files. Also added 404 route to app.js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # InteractiveArtGenerator
-A web based app that allows users to create art
+A web based app that allows users to create art using the three.js library
+
+The three.js library is use to create a 3-d environment where the user if free to load in shapes and animations
+
+This is being run on a express/node.js server with ejs templates vite as its package/production manager
+
+To run this server on our own computer, clone the repository, cd into it, run npm install and then just run npm run dev to host it on your local machine.
+
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 
-
+// imports express library, and sets it working directory to the public folder and info collection from the post method
 const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 
+// sets working directory for view engine to views folder
 app.set("view engine", "ejs");
 
 app.get("/", (req,res) => {
@@ -18,8 +19,18 @@ app.get("/home", (req,res) => {
     });
 });
 
+// If user enters an unknown address, this function will activate
+
+app.use((req,res) => {
+    res.status(404).render("404", {title: "404"});
+});
+
+// Runs server from port 3000 if in production environment
+
 if (import.meta.env.PROD){
     app.listen(3000);
 }
 
+
+// Exports it in a way that will allow vite to start the server from here
 export const viteNodeApp = app;
