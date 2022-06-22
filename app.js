eba9ca56054643a4dcf5a1225fa938565d6c6cb5
JoaquinCisneros <2014joaquincisneros@gmail.com>
2022-06-21 19:55:12 -0700

    Added ejs and three to dependancies, also aded basic template

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 import express from 'express';
 
+
+
 const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+    res.redirect("/home");
+});
+
+app.get("/home", (req,res) => {
+    res.render("index", {
+        title: 'Home'
+    });
+});
 
 if (import.meta.env.PROD){
     app.listen(3000);
