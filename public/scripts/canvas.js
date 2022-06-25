af718bb741b01a408761614e550596df50811c9d
enrique <erivera7240@gmail.com>
2022-06-25 15:08:44 -0500

    adding light and changes to dat gui code

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -27,7 +27,7 @@ const controls = new OrbitControls(camera, renderer.domElement)
 //a geometry and material object to the mesh constructor
 // Note that geometry and material should be made in the mesh constructor and not stored as separate objects
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshPhongMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
@@ -52,76 +52,25 @@ document.body.appendChild(stats.dom)
 
 //Sets up a control panel in the top right that allows you to change the start cube's dimensions and camera parameters
 const gui = new GUI()
-var settings = {
-    message: "dat.GUI",
-    checkbox: true,
-    colorA: '#FF00B4', 
-    colorB: '#22CBFF', 
-    step5: 10,
-    range: 50,
-    options:"Option 1",
-    speed:0,
-    func: function() 
-    { 
-        console.log(this.range);
-    },
-    justgooddesign: function() 
-    { 
-      window.location = "https://www.justgooddesign.com";
-    },
-    field1: "Field 1",
-    field2: "Field 2",
-    color0: "#ffae23", // CSS string
-    color1: [ 0, 128, 255 ], // RGB array
-    color2: [ 0, 128, 255, 0.3 ], // RGB with alpha
-    color3: { h: 350, s: 0.9, v: 0.3 } // Hue, saturation, value
-  };
 
-  gui.add(settings, 'message').onChange(function (value) {
-    init();
-  });  
-  gui.add(settings, 'step5', 0, 255).step(5); 
-  gui.add(settings, 'checkbox').onChange(function (value) {
-    init();
-  }); 
-  gui.add(settings, 'range', 1, 100).onChange(function (value) {
-    init();
-  });
-  gui.add(settings, 'options', [ 'Option 1', 'Option 2', 'Option 3' ] );
-  gui.add(settings, 'speed', { Low: 0, Med: 0.5, High: 1 } );
-  gui.addColor(settings, 'colorA').onChange(function (value) {
-    init();
-  });
-  gui.addColor(settings, 'colorB').onChange(function (value) {
-    init();
-  });
-  gui.add(settings, 'func');
-  gui.open();
-  
-  var f1 = gui.addFolder('Fields');
-  f1.add(settings, 'field1');
-  f1.add(settings, 'field2');
-  f1.open();
-  
-  var f2 = gui.addFolder('Colors');
-  f2.addColor(settings, 'color0');
-  f2.addColor(settings, 'color1');
-  f2.addColor(settings, 'color2');
-  f2.addColor(settings, 'color3');
-  f2.open();
-  
-  var f3 = gui.addFolder('Just Good Design'); 
-  f3.add(settings, 'justgooddesign');
-  f3.open();
 
-// const cubeFolder = gui.addFolder('Cube')
-// cubeFolder.add(cube.scale, 'x', -5, 5)
-// cubeFolder.add(cube.scale, 'y', -5, 5)
-// cubeFolder.add(cube.scale, 'z', -5, 5)
-// cubeFolder.open()
-// const cameraFolder = gui.addFolder('Camera')
-// cameraFolder.add(camera.position, 'z', 0, 10)
-// cameraFolder.open()
+const ShapeAttributesFolder = gui.addFolder('Shape Attributes')
+ShapeAttributesFolder.add(cube.scale, 'x', -5, 5)
+ShapeAttributesFolder.add(cube.scale, 'y', -5, 5)
+ShapeAttributesFolder.add(cube.scale, 'z', -5, 5)
+//ShapeAttributesFolder.add(material)
+ShapeAttributesFolder.open()
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.open()
+
+const LightFolder = gui.addFolder('Light Folder')
+LightFolder.open()
+
+//Lighting to add more realism to the scene and to use better meshes
+var Light = new THREE.AmbientLight(0x404040)
+scene.add(Light)
 
 
 //This function is special as it will continually run throughout the life of the server,
